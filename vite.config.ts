2f562ff1d6eb4440bdddccc88c70826b770c49b0
jamestothekim <james@promoraven.com>
2025-03-20 14:34:33 -0400

    update

--- vite.config.ts
+++ vite.config.ts
@@ -4,6 +4,7 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
+  base: '/',
   server: {
     proxy: {
       '/api': {
@@ -13,4 +14,9 @@ export default defineConfig({
       },
     },
   },
+  preview: {
+    port: 5173,
+    strictPort: true,
+    host: true
+  }
 })
