b9deb303fca62a1878e02aef015c47fc23b88370
jamestothekim <james@promoraven.com>
2025-05-26 22:46:58 -0400

    update

--- src/components/VolumeForecast.tsx
+++ src/components/VolumeForecast.tsx
@@ -10,9 +10,6 @@ import { VolumeForecast as MainVolumeForecast } from "../volume/volumeForecast";
 
 const VolumeForecast = () => {
   const dispatch = useDispatch();
-  const [selectedMarkets, setSelectedMarkets] = useState<string[]>([]);
-  const [selectedBrands, setSelectedBrands] = useState<string[]>([]);
-  const [selectedTags, setSelectedTags] = useState<number[]>([]);
   const [availableBrands, setAvailableBrands] = useState<string[]>([]);
   const [marketData, setMarketData] = useState<any[]>([]);
 
@@ -30,15 +27,12 @@ const VolumeForecast = () => {
 
         if (forecast_selected_markets) {
           dispatch(setVolumeForecastMarkets(forecast_selected_markets));
-          setSelectedMarkets(forecast_selected_markets);
         }
         if (forecast_selected_brands) {
           dispatch(setVolumeForecastBrands(forecast_selected_brands));
-          setSelectedBrands(forecast_selected_brands);
         }
         if (forecast_selected_tags) {
           dispatch(setVolumeForecastTags(forecast_selected_tags));
-          setSelectedTags(forecast_selected_tags);
         }
       } catch (error) {
         console.error("Error loading settings:", error);
